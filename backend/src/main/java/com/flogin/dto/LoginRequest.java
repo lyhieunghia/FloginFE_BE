9e668d0733a9393b40ab0b6b8041a123950b3f30
Phan Trọng Tiến - Ti3nz <3123410376@sv.sgu.edu.vn>
2025-11-04 18:37:53 +0700

    Implement authentication API with login endpoint and corresponding tests

--- backend/src/main/java/com/flogin/dto/LoginRequest.java
+++ backend/src/main/java/com/flogin/dto/LoginRequest.java
@@ -1,7 +1,12 @@
 package com.flogin.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+    @NotBlank(message = "username is required")
     private String username;
+    
+    @NotBlank(message = "password is required")
     private String password;
 
     public LoginRequest() {}
