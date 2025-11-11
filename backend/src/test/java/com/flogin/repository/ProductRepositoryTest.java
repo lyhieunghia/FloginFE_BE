4fbd345d1a1b2d8a4e312b3dc1a20be3bc304da6
lyhieunghia <nghiasd2020@gmail.com>
2025-11-11 08:49:20 +0700

    configure application-test.yaml to use H2 in-memory database for unit tests

--- backend/src/test/java/com/flogin/repository/ProductRepositoryTest.java
+++ backend/src/test/java/com/flogin/repository/ProductRepositoryTest.java
@@ -6,13 +6,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
 
 import com.flogin.entity.Product;
 
 @DataJpaTest
 @DisplayName("Product Repository Unit Tests")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.yaml")
 class ProductRepositoryTest {
 
     @Autowired
