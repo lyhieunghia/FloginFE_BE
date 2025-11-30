d1d89ce45635e618d5f1682f9b3bc31c6a755eea
lyhieunghia <nghiasd2020@gmail.com>
2025-11-30 11:37:56 +0700

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,12 +1,7 @@
 const { defineConfig } = require("cypress");
-const codeCoverage = require("@cypress/code-coverage/plugin");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      codeCoverage(on, config);
-      return config;
-    },
     baseUrl: "http://localhost:3000",
     defaultCommandTimeout: 10000,
     viewportWidth: 1366,
