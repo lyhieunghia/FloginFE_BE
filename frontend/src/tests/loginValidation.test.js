eec98783d6266ed2f541e5ce688bc32d778c5012
lyhieunghia <nghiasd2020@gmail.com>
2025-11-30 07:40:38 +0700

    upload coverage to codecov

--- frontend/src/tests/loginValidation.test.js
+++ frontend/src/tests/loginValidation.test.js
@@ -40,17 +40,38 @@ describe("Login Validation Tests", () => {
         expect(errors.usernameError).toBe("Username không được chứa khoảng trắng");
     });
 
-    test("TC6: Password rỗng trả về lỗi", () => {
+    test("TC6: Username chứa kí tự đặc biệt trả về lỗi", () => {
+        const username = "invalid#$%^<>";
+        const password = "ValidPass123";
+        const errors = validateLoginForm(username, password);
+        expect(errors.usernameError).toBe("Username không được chứa kí tự đặc biệt");
+    });
+
+    test("TC7: Password rỗng trả về lỗi", () => {
         const username = "validUser";
         const password = "";
         const errors = validateLoginForm(username, password);
         expect(errors.passwordError).toBe("Password không được để trống");
     });
 
-    test("TC7: Password quá ngắn trả về lỗi", () => {
+    test("TC8: Password quá ngắn trả về lỗi", () => {
         const username = "validUser";
         const password = "123";
         const errors = validateLoginForm(username, password);
         expect(errors.passwordError).toBe("Password phải có ít nhất 6 ký tự");
+    });
+
+    test("TC9: Password không có chữ cái trả về lỗi", () => {
+        const username = "validUser";
+        const password = "123123";
+        const errors = validateLoginForm(username, password);
+        expect(errors.passwordError).toBe("Password phải chứa ít nhất 1 chữ cái");
+    });
+
+    test("TC10: Password quá ngắn trả về lỗi", () => {
+        const username = "validUser";
+        const password = "abcdefgh";
+        const errors = validateLoginForm(username, password);
+        expect(errors.passwordError).toBe("Password phải chứa ít nhất 1 chữ số");
     });
 });
