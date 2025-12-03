0960220fe74b62ac66e1740cbe2659ae826d5be0
tdminhtien2005 <tdminhtien2005@gmail.com>
2025-12-03 18:30:52 +0700

    Add axios mock and improve test setup

---
+++ frontend/src/__mocks__/axios.js
@@ -0,0 +1,13 @@
+// Mock axios module for Jest tests
+export default {
+  get: jest.fn(() => Promise.resolve({ data: {} })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} })),
+  create: jest.fn(function() {
+    return this;
+  }),
+  defaults: {
+    adapter: {}
+  }
+};
